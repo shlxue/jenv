255856f076cec87318012adbe17ea14d52d97d34
Shl Xue <xueshuiliang@gmail.com>
2025-04-12 09:54:36 +0800

    Support mvnd for maven plugin

--- available-plugins/maven/etc/jenv.d/exec/maven-before.bash
+++ available-plugins/maven/etc/jenv.d/exec/maven-before.bash
@@ -1,4 +1,11 @@
-if [ "$1" = "mvn" -o "$1" = "mvnDebug"  ]; then
+if [ "$1" = "mvnd" ] && [ -n "$JENV_OPTIONS" ]; then
+  if [ -z "$MVND_HOME" ]; then
+    exportVariable MVND_HOME
+  else
+    echo "MVND_HOME is set and not overridden by jenv"
+  fi
+fi
+if [ "$1" = "mvn" -o "$1" = "mvnDebug" ]; then
   if [ -n "$JENV_OPTIONS" ]; then
     if [ -z "$MAVEN_OPTS" ]; then
       exportVariable MAVEN_OPTS "$JENV_OPTIONS"
--- available-plugins/maven/etc/jenv.d/rehash/maven.bash
+++ available-plugins/maven/etc/jenv.d/rehash/maven.bash
@@ -5,11 +5,13 @@ backuppath=$PATH
 PATH="$(remove_from_path "${JENV_ROOT}/shims")"
 MAVEN_BIN="$(command -v "mvn" || true)"
 MAVEN_DEBUG="$(command -v "mvnDebug" || true)"
+MVND_BIN="$(command -v "mvnd" || true)"
 
 
 PATH=$backuppath
 
 make_shims "$MAVEN_BIN"  
-make_shims "$MAVEN_DEBUG"  
+make_shims "$MAVEN_DEBUG"
+make_shims "$MVND_BIN"
 
 
